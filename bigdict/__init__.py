05f28d05eacbed212501f37359ae31c5c47c4f82
mewwts <mats@plysjbyen.net>
2014-12-10 08:07:57 +0100

    Added init, and moved files into package dir.

---
+++ bigdict/__init__.py
@@ -0,0 +1,9 @@
+from .big_dict import Dict
+
+
+__title__ = 'Dict'
+__version__ = '0.0.1'
+__author__ = 'Mats Julian Olsen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Mats Julian Olsen'
+__all__ = ['Dict']
---
+++ bigdict/test_bigdict.py
@@ -0,0 +1,60 @@
+import json
+import unittest
+from big_dict import Dict
+
+TEST_VAL = [1, 2, 3]
+TEST_DICT = {'a': {'b': {'c': TEST_VAL}}}
+TEST_DICT_STR = str(TEST_DICT)
+
+class Tests(unittest.TestCase):
+
+	def test_set_one_level_item(self):
+		some_dict = {'a': TEST_VAL}
+		prop = Dict()
+		prop['a'] = TEST_VAL
+		self.assertDictEqual(prop, some_dict)
+
+	def test_set_two_level_items(self):
+		some_dict = {'a': {'b': TEST_VAL}}
+		prop = Dict()
+		prop['a']['b'] = TEST_VAL
+		self.assertDictEqual(prop, some_dict)
+
+	def test_set_three_level_items(self):
+		prop = Dict()
+		prop['a']['b']['c'] = TEST_VAL
+		self.assertDictEqual(prop, TEST_DICT)
+
+	def test_set_one_level_property(self):
+		prop = Dict()
+		prop.a = TEST_VAL
+		self.assertDictEqual(prop, {'a': TEST_VAL})
+
+	def test_set_two_level_properties(self):
+		prop = Dict()
+		prop.a.b = TEST_VAL
+		self.assertDictEqual(prop, {'a': {'b': TEST_VAL}})
+
+	def test_set_three_level_properties(self):
+		prop = Dict()
+		prop.a.b.c = TEST_VAL
+		self.assertDictEqual(prop, TEST_DICT)
+
+	def test_instantiation(self):
+		self.assertDictEqual(TEST_DICT, Dict(TEST_DICT))
+
+	def test_str(self):
+		prop = Dict(TEST_DICT)
+		self.assertEqual(str(prop), str(TEST_DICT))
+
+	def test_isinstance(self):
+		self.assertTrue(isinstance(Dict(), dict))
+
+	def test_json(self):
+		some_dict = TEST_DICT
+		some_json = json.dumps(some_dict)
+		prop = Dict()
+		prop.a.b.c = TEST_VAL
+		prop_json = json.dumps(prop)
+		self.assertEqual(some_json, prop_json)
+
