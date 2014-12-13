b4154ec7dbe21a59bae8f5cd40359bfd14c7dc84
mewwts <mats@plysjbyen.net>
2014-12-13 23:10:48 +0100

    version bumpt to 0.2.0

--- addict/__init__.py
+++ addict/__init__.py
@@ -2,7 +2,7 @@ from .addict import Dict
 
 
 __title__ = 'addict'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Mats Julian Olsen'
