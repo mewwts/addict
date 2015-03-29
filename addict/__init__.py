5f31cc1d4031aaa1a416261bf456e0a1e6a93f58
mewwts <mats@plysjbyen.net>
2015-03-29 18:56:20 +0200

    Version bump. Deepcopy bumps it up to 0.4.0

--- addict/__init__.py
+++ addict/__init__.py
@@ -2,7 +2,7 @@ from .addict import Dict
 
 
 __title__ = 'addict'
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 __author__ = 'Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Mats Julian Olsen'
