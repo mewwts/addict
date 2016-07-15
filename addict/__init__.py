2ef6ab8e50df023984e76f9f3b404285afc4954d
Mats Julian Olsen <(none)>
2016-07-15 16:38:30 +0200

    1.0.0

--- addict/__init__.py
+++ addict/__init__.py
@@ -2,8 +2,8 @@ from .addict import Dict
 
 
 __title__ = 'addict'
-__version__ = '0.5.1'
+__version__ = '1.0.0'
 __author__ = 'Mats Julian Olsen'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2014 Mats Julian Olsen'
+__copyright__ = 'Copyright 2014, 2015, 2016 Mats Julian Olsen'
 __all__ = ['Dict']
