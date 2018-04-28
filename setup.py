c2747561db09b40e6ea1e278905068acc351c3b5
Mats Julian Olsen <mats@plysjbyen.net>
2018-04-28 15:04:05 +0200

    3.7 (#97)
    Test against 3.7 nightly builds.

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
