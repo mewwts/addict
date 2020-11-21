338936265dd924ec5892aeee9c122d7e4f77680d
Mats <mats@plysjbyen.net>
2020-11-21 17:17:03 +0100

    2.4.0: freeze/unfreeze

--- addict/__init__.py
+++ addict/__init__.py
@@ -3,8 +3,8 @@ from .addict import Dict as Addict
 
 
 __title__ = 'addict'
-__version__ = '2.2.1'
+__version__ = '2.4.0'
 __author__ = 'Mats Julian Olsen'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2014, 2015, 2016 Mats Julian Olsen'
+__copyright__ = 'Copyright 2014-2020 Mats Julian Olsen'
 __all__ = ['Dict']
--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
